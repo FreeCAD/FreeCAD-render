60177fae4abd4cee8ad9913a4fe22a83c8938209
howetuft <howetuft@gmail.com>
2021-06-20 17:00:04 +0200

    Create migration modules for camera and lights

---
+++ camera.py
@@ -0,0 +1,9 @@
+# This is simply a migration module, as lights.py has been moved, and it
+# should not be modified.
+
+"""A migration module for Camera."""
+
+import Render
+
+Camera = Render.Camera
+ViewProviderCamera = Render.ViewProviderCamera
