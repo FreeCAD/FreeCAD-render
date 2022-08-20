07b0701488343434e09c97c0f452fca9f1d06aae
howetuft <howetuft@gmail.com>
2022-08-20 17:55:27 +0200

    Cycles: update copyright

--- Render/renderers/Cycles.py
+++ Render/renderers/Cycles.py
@@ -1,6 +1,7 @@
 # ***************************************************************************
 # *                                                                         *
 # *   Copyright (c) 2019 Yorik van Havre <yorik@uncreated.net>              *
+# *   Copyright (c) 2022 Howefuft <howetuft-at-gmail>                       *
 # *                                                                         *
 # *   This program is free software; you can redistribute it and/or modify  *
 # *   it under the terms of the GNU Lesser General Public License (LGPL)    *
