d6c24d4aa311d1251da4c2de3031a7e6b6112822
howetuft <howetuft@gmail.com>
2022-10-11 21:47:14 +0200

    Docs: replace marked submodule by mere .js file

--- Render/help.py
+++ Render/help.py
@@ -1,13 +1,14 @@
 from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEngineScript
 from PySide2.QtCore import QUrl
 import FreeCADGui as Gui
+import FreeCAD as App
 
 from Render.constants import WBDIR
 
 
 def open_help():
-    if not SCRIPT_JQUERY or not SCRIPT_MARKED:
-        raise FileNotFoundError("Could not find jQuery.js or Marked.js")
+    if not SCRIPT_JQUERY or not SCRIPT_MARKED or not App.GuiUp:
+        return
 
     viewer = QWebEngineView()
     scripts = viewer.page().scripts()
@@ -42,7 +43,7 @@ SCRIPT_JQUERY, SCRIPT_MARKED = None, None
 with open(f"{WBDIR}/docs/3rdparty/jQuery.js") as f:
     SCRIPT_JQUERY = f.read()
 
-with open(f"{WBDIR}/docs/3rdparty/marked/marked.min.js") as f:
+with open(f"{WBDIR}/docs/3rdparty/marked.min.js") as f:
     SCRIPT_MARKED = f.read()
 
 SCRIPT_RUN = f"""\
