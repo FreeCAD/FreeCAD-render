1d1379ba8b4624e69880150742b6d3769e8b6ee8
howetuft <howetuft@gmail.com>
2022-10-12 22:55:26 +0200

    Docs: add toolbar to helpviewer

--- Render/help.py
+++ Render/help.py
@@ -22,51 +22,94 @@
 
 """This module implements a help viewer for Render workbench."""
 
-from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEngineScript
+from PySide2.QtWebEngineWidgets import (
+    QWebEngineView,
+    QWebEngineScript,
+    QWebEnginePage,
+)
 from PySide2.QtCore import QUrl
+from PySide2.QtWidgets import QWidget, QToolBar, QVBoxLayout
+
 import FreeCADGui as Gui
 import FreeCAD as App
 
 from Render.constants import WBDIR
 
 
+class HelpViewer(QWidget):
+    """A help viewer widget.
+
+    The help viewer is an html viewer but is able to render (local)markdown
+    files.
+    Markdown files are converted on-the-fly, thanks to 'marked.js' module.
+    (https://github.com/markedjs/marked)
+    """
+
+    def __init__(self, parent=None):
+        """Initialize HelpViewer."""
+        assert SCRIPT_JQUERY and SCRIPT_MARKED
+
+        super().__init__(parent)
+
+        self.setLayout(QVBoxLayout())
+
+        # Set subwidgets
+        self.toolbar = QToolBar()
+        self.layout().addWidget(self.toolbar)
+        self.view = QWebEngineView()
+        self.layout().addWidget(self.view)
+
+        # Add actions to toolbar
+        self.toolbar.addAction(self.view.pageAction(QWebEnginePage.Back))
+        self.toolbar.addAction(self.view.pageAction(QWebEnginePage.Forward))
+        self.toolbar.addAction(self.view.pageAction(QWebEnginePage.Reload))
+        self.toolbar.addAction(self.view.pageAction(QWebEnginePage.Stop))
+
+        # Insert scripts into Web view
+        scripts = self.view.page().scripts()
+
+        script_jquery = QWebEngineScript()
+        script_jquery.setSourceCode(SCRIPT_JQUERY)
+        script_jquery.setInjectionPoint(QWebEngineScript.DocumentCreation)
+        scripts.insert(script_jquery)
+
+        script_marked = QWebEngineScript()
+        script_marked.setSourceCode(SCRIPT_MARKED)
+        script_marked.setInjectionPoint(QWebEngineScript.DocumentCreation)
+        scripts.insert(script_marked)
+
+        script_run = QWebEngineScript()
+        script_run.setSourceCode(SCRIPT_RUN)
+        script_run.setInjectionPoint(QWebEngineScript.DocumentReady)
+        scripts.insert(script_run)
+
+    def setUrl(self, url):  # pylint: disable=invalid-name
+        """Set viewer url.
+
+        Args:
+            url -- url to set viewer to (QUrl)
+        """
+        self.view.setUrl(url)
+
+
 def open_help():
     """Open a help viewer on Render documentation.
 
-    The help viewer is an html viewer. The documentation files are markdown
-    files, they are converted on-the-fly, thanks to marked.js module
-    (https://github.com/markedjs/marked)
     The help files are located in ./docs directory, except the root file, which
     is in the workbench root directory. As the files are located in local
     files, the help is available off-line.
+    Help files are in markdown format.
     """
-    if not SCRIPT_JQUERY or not SCRIPT_MARKED or not App.GuiUp:
+    if not App.GuiUp:
         return
 
-    viewer = QWebEngineView()
-    scripts = viewer.page().scripts()
-
-    script_jquery = QWebEngineScript()
-    script_jquery.setSourceCode(SCRIPT_JQUERY)
-    script_jquery.setInjectionPoint(QWebEngineScript.DocumentCreation)
-    scripts.insert(script_jquery)
-
-    script_marked = QWebEngineScript()
-    script_marked.setSourceCode(SCRIPT_MARKED)
-    script_marked.setInjectionPoint(QWebEngineScript.DocumentCreation)
-    scripts.insert(script_marked)
-
-    script_run = QWebEngineScript()
-    script_run.setSourceCode(SCRIPT_RUN)
-    script_run.setInjectionPoint(QWebEngineScript.DocumentReady)
-    scripts.insert(script_run)
-
+    viewer = HelpViewer()
     mdiarea = Gui.getMainWindow().centralWidget()
     subw = mdiarea.addSubWindow(viewer)
     subw.setWindowTitle("Render help")
     subw.setVisible(True)
 
-    viewer.setUrl(URL)
+    viewer.setUrl(QUrl(f"file://{WBDIR}/README.md"))
     viewer.show()
 
 
@@ -79,12 +122,10 @@ with open(f"{WBDIR}/docs/3rdparty/jQuery.js", encoding="utf-8") as f:
 with open(f"{WBDIR}/docs/3rdparty/marked.min.js", encoding="utf-8") as f:
     SCRIPT_MARKED = f.read()
 
-SCRIPT_RUN = f"""\
+SCRIPT_RUN = """\
   // @match file://*/*.md
-  $.when( $.ready).then(function() {{
+  $.when( $.ready).then(function() {
     var now_body = $("body").text();
     $("body").html( marked.parse(now_body) );
-    }});
+    });
 """
-
-URL = QUrl(f"file://{WBDIR}/README.md")
