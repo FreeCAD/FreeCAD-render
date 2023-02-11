dc1cdfe2889022419f5bd1d1d35289beec984a43
howetuft <howetuft@gmail.com>
2023-02-11 14:38:11 +0100

    Rendermesh: uvmap_cube -- remove unused function

--- Render/rendermesh_mp/uvmap_cube.py
+++ Render/rendermesh_mp/uvmap_cube.py
@@ -69,11 +69,6 @@ def getfacet(idx):
     return SHARED_FACETS[idx], SHARED_FACETS[idx + 1], SHARED_FACETS[idx + 2]
 
 
-# TODO Remove
-def transform_points(matrix, points):
-    """Transform points with a transformation matrix 4x4."""
-    return [transform(matrix, point) for point in points]
-
 # *****************************************************************************
 
 
