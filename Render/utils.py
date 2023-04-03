39ed8d65603ae1999814089e32d31f4be339229c
howetuft <howetuft@gmail.com>
2023-04-03 10:45:42 +0200

    Utils.grouper: compatibility with Python 3.6

--- Render/utils.py
+++ Render/utils.py
@@ -272,21 +272,18 @@ def clear_report_view():
     text_widget.clear()
 
 
-def grouper(iterable, number, *, incomplete="strict", fillvalue=None):
+def grouper(iterable, number, *, incomplete="ignore", fillvalue=None):
     "Collect data into non-overlapping fixed-length chunks or blocks"
     # From Python documentation (itertools module)
     # grouper('ABCDEFG', 3, fillvalue='x') --> ABC DEF Gxx
-    # grouper('ABCDEFG', 3, incomplete='strict') --> ABC DEF ValueError
     # grouper('ABCDEFG', 3, incomplete='ignore') --> ABC DEF
     args = [iter(iterable)] * number
     if incomplete == 'fill':
         return itertools.zip_longest(*args, fillvalue=fillvalue)
-    if incomplete == 'strict':
-        return zip(*args, strict=True)
     if incomplete == 'ignore':
         return zip(*args)
     else:
-        raise ValueError('Expected fill, strict, or ignore')
+        raise ValueError('Expected fill or ignore')
 
 
 class SharedWrapper:
