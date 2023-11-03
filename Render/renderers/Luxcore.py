5da3ad423fb67ffcc1e04033ade8a6b13d22967d
howetuft <howetuft@gmail.com>
2023-11-03 15:48:23 +0100

    DistantLight: Luxcore

--- Render/renderers/Luxcore.py
+++ Render/renderers/Luxcore.py
@@ -274,6 +274,19 @@ scene.lights.{n}.gamma = 1.0
     return dedent(snippet).format(n=name, f=image)
 
 
+def write_distantlight(name, color, power, direction, **_):
+    """Compute a string in renderer SDL to represent an image-based light."""
+    color = color.to_srgb()
+    snippet = f"""
+# Distant light '{name}'
+scene.lights.{name}.type = sharpdistant
+scene.lights.{name}.color = luxcore 2.2 {color[0]} {color[1]} {color[2]}
+scene.lights.{name}.direction = {direction.x} {direction.y} {direction.z}
+scene.lights.{name}.gain = {power} {power} {power}
+"""
+    return dedent(snippet)
+
+
 # ===========================================================================
 #                              Material implementation
 # ===========================================================================
