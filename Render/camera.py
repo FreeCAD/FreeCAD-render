ff60582958087752a759e7dbfc8d9c8938015bf1
howetuft <howetuft@gmail.com>
2024-07-03 19:36:27 +0200

    License: move LGPL 2 (obsolete) to LGPL 2.1

--- Render/camera.py
+++ Render/camera.py
@@ -4,7 +4,7 @@
 # *                                                                         *
 # *   This program is free software; you can redistribute it and/or modify  *
 # *   it under the terms of the GNU Lesser General Public License (LGPL)    *
-# *   as published by the Free Software Foundation; either version 2 of     *
+# *   as published by the Free Software Foundation; either version 2.1 of   *
 # *   the License, or (at your option) any later version.                   *
 # *   for detail see the LICENCE text file.                                 *
 # *                                                                         *
